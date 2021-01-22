ff3b662810f545878f74c3a712e4b2a7e5742cec
Yu He <yuhe00@gmail.com>
2021-01-22 17:32:31 +0100

    Fixes to specifiers in settings object

--- Source/Gridly/Public/GridlyGameSettings.h
+++ Source/Gridly/Public/GridlyGameSettings.h
@@ -4,7 +4,8 @@
 
 #include "GridlyGameSettings.generated.h"
 
-UCLASS(BlueprintType, Config = Game, DefaultConfig)
+UCLASS(BlueprintType, Config = Game, DefaultConfig,
+	AutoExpandCategories=("Gridly|Import Settings", "Gridly|Export Settings", "Gridly|Options"))
 class GRIDLY_API UGridlyGameSettings final : public UObject
 {
 	GENERATED_BODY()
@@ -17,27 +18,28 @@ public:
 	/** The view IDs to fetch from Gridly. Record IDs will be combined. Duplicate keys will be ignored. */
 	UPROPERTY(Category = "Gridly|Import Settings", BlueprintReadOnly, EditAnywhere, Config)
 	TArray<FString> ImportFromViewIds;
-	
+
 	/** The view ID to export the source strings to. */
 	UPROPERTY(Category = "Gridly|Export Settings", BlueprintReadOnly, EditAnywhere, Config)
 	FString ExportApiKey;
-	
+
 	/** The view ID to export the source strings to. */
 	UPROPERTY(Category = "Gridly|Export Settings", BlueprintReadOnly, EditAnywhere, Config)
 	FString ExportViewId;
-	
+
 	/** Use combined comma-separated "{namespace},{key}" as record ID. WARNING! This should not be changed after a project has already been exported */
 	UPROPERTY(Category = "Gridly|Options", BlueprintReadOnly, EditAnywhere, Config)
 	bool bUseCombinedNamespaceId = false;
-	
+
 	/** Set to "path" to use Gridly's path tag functionality for namespaces. This can also be mapped to any other column of the string type.  */
-	UPROPERTY(Category = "Gridly|Options", BlueprintReadOnly, EditAnywhere, Config, meta = (EditCondition="!bUseCombinedNamespaceId"))
+	UPROPERTY(Category = "Gridly|Options", BlueprintReadOnly, EditAnywhere, Config,
+		meta = (EditCondition="!bUseCombinedNamespaceId"))
 	FString NamespaceColumnId = "path";
-	
+
 	/** Column ID prefix for source language columns on Gridly */
 	UPROPERTY(Category = "Gridly|Options", BlueprintReadOnly, EditAnywhere, Config)
 	FString SourceLanguageColumnIdPrefix = "src_";
-	
+
 	/** Column ID prefix for target language columns on Gridly */
 	UPROPERTY(Category = "Gridly|Options", BlueprintReadOnly, EditAnywhere, Config)
 	FString TargetLanguageColumnIdPrefix = "tg_";
@@ -47,12 +49,13 @@ public:
 	bool bUseCustomCultureMapping = true;
 
 	/** This will remap locale settings from Unreal to Gridly. Unreal uses "en-US", while Gridly generally uses "enUS". However, this mapping can be modified to suit the project's needs. */
-	UPROPERTY(Category = "Gridly|Options", BlueprintReadOnly, EditAnywhere, Config, meta = (EditCondition="bUseCustomCultureMapping"))
+	UPROPERTY(Category = "Gridly|Options", BlueprintReadOnly, EditAnywhere, Config,
+		meta = (EditCondition="bUseCustomCultureMapping"))
 	TMap<FString, FString> CustomCultureMapping;
 
 public:
 	UGridlyGameSettings();
-	
+
 public:
 	static bool OnSettingsSaved();
 };
